f4159f9d391d80714ea7b5b45008fd1b8d7f47bb
TomPyer <jizhidetangxuelin@outlook.com>
2016-08-29 09:04:09 -0700

    mysql success

--- myblog.py
+++ myblog.py
@@ -214,6 +214,7 @@ def logout():
         return redirect(url_for('login'))
     else:
         return redirect(url_for('login'))
+        pass
 
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
