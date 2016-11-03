29897b1d16da25b86cc1d0bd878e750668b9d099
TomPyer <jizhidetangxuelin@outlook.com>
2016-11-03 07:18:16 -0700

    mysql success

--- myblog.py
+++ myblog.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for('welcome'))
 
 @app.route('/welcome',methods=['GET','POST'])
 def welcome():
